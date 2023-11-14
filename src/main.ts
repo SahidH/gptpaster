1f126f327e474f2cdba8a9853babee744c7b10cf
Sahid Hernandez <sahidhr@gmail.com>
2023-11-14 11:55:25 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
-import './style.css';
+import "./style.css";
 
-const resultElement = document.getElementById('result');
-const inputElement = document.getElementById('pasteBox');
+const resultElement = document.getElementById("result") as HTMLDivElement;
+const inputElement = document.getElementById("pasteBox") as HTMLDivElement;
+const copyButtonElement = document.getElementById(
+  "copyButton"
+) as HTMLButtonElement;
+const statusElement = document.getElementById("status") as HTMLSpanElement;
 
 const handlePaste = (e: ClipboardEvent) => {
-  const htmlData = e.clipboardData?.getData('text/html');
+  const htmlData = e.clipboardData?.getData("text/html");
   if (!htmlData) return;
-
+  inputElement.innerHTML = htmlData;
   const parser = new DOMParser();
-  const htmlDoc = parser.parseFromString(htmlData, 'text/html');
+  const htmlDoc = parser.parseFromString(htmlData, "text/html");
   const findAndReturnATag = (node: ChildNode): ChildNode[] => {
-    if (node.nodeName === 'A') {
+    if (node.nodeName === "A") {
       if (!node.textContent) {
         node.textContent = '["]';
       }
@@ -23,12 +27,12 @@ const handlePaste = (e: ClipboardEvent) => {
     }, [] as ChildNode[]);
   };
 
-  const stripStyles = (node: ChildNode) => {
+  const stripStyles = (node: ChildNode | HTMLElement) => {
     if (node.nodeType === 1) {
       if (!node.textContent) {
         const aNodes = findAndReturnATag(node);
         if (aNodes.length) {
-          const spanTag = document.createElement('span');
+          const spanTag = document.createElement("span");
           /** node replace inner childs to aNodes elements */
           aNodes.forEach((aNode) => {
             stripStyles(aNode);
@@ -41,13 +45,15 @@ const handlePaste = (e: ClipboardEvent) => {
         }
       } else {
         // Element node
-        node.removeAttribute('style'); // Remove style attribute
-        Array.from(node.attributes).forEach((attr) => {
-          if (attr.name !== 'href') {
-            // Preserve href attributes
-            node.removeAttribute(attr.name); // Remove other attributes
-          }
-        });
+        if ("removeAttribute" in node) {
+          node.removeAttribute("style"); // Remove style attribute
+          Array.from(node.attributes).forEach((attr) => {
+            if (attr.name !== "href") {
+              // Preserve href attributes
+              node.removeAttribute(attr.name); // Remove other attributes
+            }
+          });
+        }
       }
     }
     Array.from(node.childNodes).forEach(stripStyles); // Recurse through child nodes
@@ -62,5 +68,29 @@ const handlePaste = (e: ClipboardEvent) => {
 };
 
 if (inputElement) {
-  inputElement.addEventListener('paste', handlePaste);
+  inputElement.addEventListener("paste", handlePaste);
+}
+
+if (copyButtonElement) {
+  copyButtonElement.addEventListener("click", () => {
+    statusElement.innerText = "⏳";
+    navigator.clipboard
+      .writeText(resultElement.innerHTML)
+      .then(() => {
+        if (statusElement) {
+          statusElement.innerText = "✅";
+          setTimeout(() => {
+            statusElement.innerText = "📋";
+          }, 2000);
+        }
+      })
+      .catch(() => {
+        if (statusElement) {
+          statusElement.innerText = "❌";
+          setTimeout(() => {
+            statusElement.innerText = "📋";
+          }, 2000);
+        }
+      });
+  });
 }
